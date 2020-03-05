870509a9292b5f86aa704b0cb56669262b961261
Chris Eustis <ceustis@penbaysolutions.com>
2020-03-04 22:11:41 -0600

    Add the .NET Core API with a single (empty) controller

---
+++ coreApi/coreApi/Controllers/HackerNewsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreApi.Controllers
+{
+    [Route("api/[controller]")]
+	[Produces("application/json")]
+	[ApiController]
+    public class HackerNewsController : ControllerBase
+    {
+    }
+}
