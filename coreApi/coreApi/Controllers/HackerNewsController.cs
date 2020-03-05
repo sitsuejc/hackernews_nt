e4b56e99ca64a062f37d4e3a9c4b625d5529bd62
Chris Eustis <ceustis@penbaysolutions.com>
2020-03-04 22:40:37 -0600

    Create hackerNews request manager to manage API requests w/ single endpoint
    * Single API endpoint for getting the best story IDs from the Hacker News API
    * Offloads the Hacker News API requests to a manger class, using DI
    into the API controller
    * Use RestSharp for making network requests
    * Updates default launch page when debugging

--- coreApi/coreApi/Controllers/HackerNewsController.cs
+++ coreApi/coreApi/Controllers/HackerNewsController.cs
@@ -1,16 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using CoreApi.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace coreApi.Controllers
+namespace CoreApi.Controllers
 {
-    [Route("api/[controller]")]
+	[Route("api/[controller]")]
 	[Produces("application/json")]
 	[ApiController]
-    public class HackerNewsController : ControllerBase
-    {
-    }
+	public class HackerNewsController : ControllerBase
+	{
+		private readonly IHackerNewsRequestManager _hackerNewsRequestManager;
+
+		public HackerNewsController(IHackerNewsRequestManager hackerNewsRequestManager)
+		{
+			_hackerNewsRequestManager = hackerNewsRequestManager;
+		}
+
+		/// <summary>
+		/// Return the IDs for the current Best Stories on Hacker News.
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet("BestStoryIds")]
+		public async Task<ActionResult<IEnumerable<int>>> GetBestStoryIds()
+		{
+			List<int> ids = await _hackerNewsRequestManager.GetBestStoryIds();
+			if (ids == null)
+			{
+				return BadRequest();
+			}
+			return Ok(ids);
+		}
+	}
 }
