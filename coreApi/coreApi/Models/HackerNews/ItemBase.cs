905456fefa00cc515e015b84646f7d0531bb33c1
Chris Eustis <ceustis@penbaysolutions.com>
2020-03-04 23:16:22 -0600

    Add endpoint for getting story details

---
+++ coreApi/coreApi/Models/HackerNews/ItemBase.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreApi.Models.HackerNews
+{
+	public class ItemBase
+	{
+		public int Id { get; set; }
+	}
+}
---
+++ coreApi/coreApi/Models/HackerNews/Story.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreApi.Models.HackerNews
+{
+	public class Story : ItemBase
+	{
+		public string By { get; set; }
+
+		public int Descendants { get; set; }
+
+		public List<int> Kids { get; set; }
+
+		public int Score { get; set; }
+
+		public int Time { get; set; }
+
+		public string Title { get; set; }
+
+		// TODO: Change to enum
+		public string Type { get; set; }
+
+		public string Url { get; set; }
+	}
+}
