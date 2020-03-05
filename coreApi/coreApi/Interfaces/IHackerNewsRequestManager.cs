4ef05eed2b6255b4a211e6b32787a0165613ecba
Chris Eustis <ceustis@penbaysolutions.com>
2020-03-04 22:40:37 -0600

    Create hackerNews request manager to manage API requests w/ single endpoint
    * Single API endpoint for getting the best story IDs from the Hacker News API
    * Offloads the Hacker News API requests to a manger class, using DI
    into the API controller
    * Use RestSharp for making network requests
    * Updates default launch page when debugging

---
+++ coreApi/coreApi/Interfaces/IHackerNewsRequestManager.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoreApi.Interfaces
+{
+	public interface IHackerNewsRequestManager
+	{
+		Task<List<int>> GetBestStoryIds();
+	}
+}
---
+++ coreApi/coreApi/Managers/HackerNewsRequestManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoreApi.Interfaces;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace CoreApi.Managers
+{
+	public class HackerNewsRequestManager : IHackerNewsRequestManager
+	{
+		private readonly string _apiRoot = "https://hacker-news.firebaseio.com/";
+		private readonly string _apiVersion = "v0";
+		private readonly string _printParam = "print=pretty";
+
+		private string BaseUrl
+		{
+			get
+			{
+				return $"{_apiRoot}/{_apiVersion}";
+			}
+		}
+
+		/// <summary>
+		/// Returns a list of IDs that are the current Best Stories from Hacker News.
+		/// If the request fails, null is returned.
+		/// </summary>
+		/// <returns></returns>
+		public async Task<List<int>> GetBestStoryIds()
+		{
+			// Generate and execute the API request
+			IRestResponse response = await ExecuteGet($"{BaseUrl}/beststories.json?{_printParam}");
+			if (!response.IsSuccessful)
+			{
+				// TODO: Log the error
+				Console.WriteLine(response.Content);
+				return null;
+			}
+
+			// The API returns an array of numbers, so deserialize to that
+			return JsonConvert.DeserializeObject<List<int>>(response.Content);
+		}
+
+		private async Task<IRestResponse> ExecuteGet(string url)
+		{
+			RestClient client = new RestClient(url);
+			RestRequest request = new RestRequest(Method.GET);
+			return await client.ExecuteAsync(request);
+		}
+	}
+}
