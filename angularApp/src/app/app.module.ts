df8646508d56e499c66b6c0121fa3a0fb4d345c7
Chris Eustis <ceustis@penbaysolutions.com>
2020-03-05 13:36:15 -0600

    Add search capability with some style cleanup
    * Adds search input into toolbar
    * Fixes sizing of cards
    * Added loading indicator during requests

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
