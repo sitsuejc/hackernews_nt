6314f0d5f386c5fe6a56afa9dc3633d1a21440b4
Chris Eustis <ceustis@penbaysolutions.com>
2020-03-05 00:32:39 -0600

    Create service for HN API requests

--- angularApp/src/app/app.component.ts
+++ angularApp/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HackerNewsService } from './services/hacker-news/hacker-news.service';
+import { Story } from './models/hacker-news/story.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appTitle = 'Nextech - Hacker News';
+
+  public bestStories: Story[] = [];
+
+  constructor(
+    private readonly hackerNewsService: HackerNewsService
+  ) { }
+
+  ngOnInit(): void {
+    this.hackerNewsService.getBestStories().subscribe(
+      stories => this.bestStories = stories,
+      console.error
+    );
+  }
 }
